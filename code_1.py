18959970eb6e45ddc2bb2f49d3569efdae49cd62
kolpaberit <143887775+kolpaberit@users.noreply.github.com>
2023-11-27 08:18:08 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,56 @@
+        data.push(Math.floor(Math.random() * 100) + 1);
+
+    data.forEach(item => {
+    });
+
+main();
+        console.log("Random Number: " + item);
+}
+    const data = generateRandomData();
+function generateRandomData() {
+    const data = [];
+}
+
+function main() {
+    return data;
+    for (let i = 0; i < 10; i++) {
+    }import random
+if __name__ == "__main__":
+def generate_random_data():
+    main()
+
+        print(f"Random Number: {item}")
+
+    return data
+    data = generate_random_data()
+    for item in data:
+
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+
+import random
+    for item in data:
+    main()
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    data = generate_random_data()
+if __name__ == "__main__":
+
+def main():
+        print(f"Random Number: {item}")
+    for item in data:
+import random
+def main():
+        print(f"Random Number: {item}")
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    main()
+def generate_random_data():
+if __name__ == "__main__":
+
+    return data
+    data = generate_random_data()
